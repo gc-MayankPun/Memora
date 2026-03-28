a9171f7ea14089f08b357e43ff2d5174e563c533
gc-MayankPun <punmayank35@gmail.com>
2026-03-29 00:21:13 +0530

    feat: Added cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,19 @@
 import savesRouter from "./routes/saves.routes.js";
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.use("/api/saves", savesRouter);
 
