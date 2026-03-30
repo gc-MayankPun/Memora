854dc8cc5c2c7f20ef9ab5dfbdb9ae75a8295fc0
gc-MayankPun <punmayank35@gmail.com>
2026-03-30 21:45:13 +0530

    fix: SPA fallback LAST

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -28,4 +28,8 @@ app.use("/api/collections", collectionRouter);
 
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+});
+
 export default app;
