167da9e833a307530afbb8bb235aaa521d879963
gc-MayankPun <punmayank35@gmail.com>
2026-03-31 17:24:15 +0530

    fix: import statement for brevo

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,4 +1,6 @@
-import SibApiV3Sdk from "@getbrevo/brevo";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const SibApiV3Sdk = require("@getbrevo/brevo");
 
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 apiInstance.authentications["apiKey"].apiKey = process.env.BREVO_API_KEY;
