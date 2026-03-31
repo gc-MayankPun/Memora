98ba1eebb56bc3e80cc4a033437d01d6004e6fc0
gc-MayankPun <punmayank35@gmail.com>
2026-03-31 17:16:26 +0530

    feat: replaced nodemailer service with Brevo

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,35 +1,17 @@
-import nodemailer from "nodemailer";
+import * as Brevo from "@getbrevo/brevo";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.BREVO_SMTP_USER,
-    pass: process.env.BREVO_SMTP_PASS,
-  },
-});
-
-transporter
-  .verify()
-  .then(() => {
-    console.log("Email transporter is ready to send emails");
-  })
-  .catch((err) => {
-    console.error("Email transporter verification failed:", err);
-  });
+const apiInstance = new Brevo.TransactionalEmailsApi();
+apiInstance.authentications["apiKey"].apiKey = process.env.BREVO_API_KEY;
 
 export async function sendEmail({ to, subject, html, text }) {
-  const mailOptions = {
-    from: `Memora <${process.env.BREVO_SMTP_USER}>`,
-    to,
-    subject,
-    html,
-    text,
-  };
+  const sendSmtpEmail = new Brevo.SendSmtpEmail();
 
-  const details = await transporter.sendMail(mailOptions); 
+  sendSmtpEmail.sender = { name: "Memora", email: process.env.BREVO_SMTP_USER };
+  sendSmtpEmail.to = [{ email: to }];
+  sendSmtpEmail.subject = subject;
+  sendSmtpEmail.htmlContent = html;
+
+  const details = await apiInstance.sendTransacEmail(sendSmtpEmail);
+  console.log("Email sent:", details);
   return details;
-}
-
-export default transporter;
+} 
